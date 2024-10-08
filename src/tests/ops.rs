399f6e5c5f6520ed0659ceed48719a28a3531564
raphaelDkhn <113879115+raphaelDkhn@users.noreply.github.com>
2024-10-08 18:36:00 +0300

    test relu and linear nn

--- src/tests/ops.rs
+++ src/tests/ops.rs
@@ -1,4 +1,6 @@
-use luminal::prelude::*;
+use dfdx::prelude::{Module as DfdxModule, *};
+use luminal::{module::Module, prelude::*};
+use luminal_nn::{Linear, ReLU};
 use rand::{rngs::StdRng, SeedableRng};
 
 use crate::{binary_test, unary_test, CairoCompiler};
@@ -217,4 +219,59 @@ fn test_matmul_transpose() {
     assert_close(&a_b_t.data(), &d_a_b_t.as_vec());
     assert_close(&a_t_b.data(), &d_a_t_b.as_vec());
     assert_close(&a_t_b_t.data(), &d_a_t_b_t.as_vec());
+}
+
+// =============== NN ===============
+#[test]
+fn test_relu_and_linear() {
+    // Test single and batch, unoptimized and optimized
+    let mut cx = Graph::new();
+    let input_data = random_vec(32);
+    let w1 = random_vec(32 * 64);
+    let w2 = random_vec(32 * 64);
+    let batch = cx.named_tensor("Batch", (2, 32)).set(random_vec(32 * 2));
+    let a = cx.named_tensor("Single", 32).set(input_data.clone());
+
+    let model = (
+        Linear::new(32, 64, false, &mut cx),
+        ReLU,
+        Linear::new(64, 32, false, &mut cx),
+    );
+    model.0.weight.set(w1.clone());
+    model.2.weight.set(w2.clone());
+    let mut b = model.forward(a).retrieve();
+    let mut batch_out = model.forward(batch).retrieve();
+    cx.execute();
+
+    let unoptimized_b = b.data();
+    let unoptimized_batch_out = batch_out.data();
+    b.drop();
+    batch_out.drop();
+    let _ = cx.compile(
+        <(GenericCompiler, CairoCompiler)>::default(),
+        (&mut b, &mut batch_out),
+    );
+    cx.execute();
+
+    assert_close_precision(&unoptimized_b, &b.data(), 1e-2);
+    assert_close_precision(&unoptimized_batch_out, &batch_out.data(), 1e-2);
+
+    // Test against dfdx
+    let dev = Cpu::default();
+    let mut model = <(
+        dfdx::nn::modules::builders::UnbiasedLinear<32, 64>,
+        dfdx::nn::modules::builders::ReLU,
+        dfdx::nn::modules::builders::UnbiasedLinear<64, 32>,
+    )>::build_on_device(&dev);
+    // Set weights
+    model.0.weight = dev
+        .tensor_from_vec(w1, (DConst::<32>, DConst::<64>))
+        .permute();
+    model.2.weight = dev
+        .tensor_from_vec(w2, (DConst::<64>, DConst::<32>))
+        .permute();
+    let a = dev.tensor_from_vec(input_data, (DConst::<32>,));
+    let out = model.forward(a);
+
+    assert_close_precision(&unoptimized_b, &out.as_vec(), 1e-2);
 }
