51f654c1d56da1b381324d79c4ca5fb7fe3bfeaf
raphaelDkhn <113879115+raphaelDkhn@users.noreply.github.com>
2024-10-14 11:48:44 +0300

    fmt

--- src/constants.rs
+++ src/constants.rs
@@ -1,12 +1,13 @@
+use lazy_static::lazy_static;
 use std::env;
 use std::path::PathBuf;
-use lazy_static::lazy_static;
-
 
 lazy_static! {
     pub static ref COMPILED_CAIRO_PATH: PathBuf = {
         env::var("LUMINAIR_COMPILED_CAIRO_PATH")
             .map(PathBuf::from)
-            .unwrap_or_else(|_| PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("cairo/compiled_ops"))
+            .unwrap_or_else(|_| {
+                PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("cairo/compiled_ops")
+            })
     };
 }
