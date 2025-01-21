124f055688d1981ce75a26ae678463760963c2ef
raphaelDkhn <113879115+raphaelDkhn@users.noreply.github.com>
2025-01-21 14:32:17 +0200

    gen add trace

--- crates/compiler/src/lib.rs
+++ crates/compiler/src/lib.rs
@@ -1,19 +1,8 @@
-use std::path::PathBuf;
-
-use luminal::prelude::GenericCompiler;
-use prim::PrimitiveCompiler;
-
-pub mod utils;
 pub mod data;
 pub mod prim;
+pub mod utils;
 
 #[cfg(test)]
 mod tests;
 
 pub type StwoCompiler = (prim::PrimitiveCompiler,);
-
-pub fn init_compiler(trace_registry: Option<PathBuf>) -> (GenericCompiler, StwoCompiler) {
-    let config = prim::Config { trace_registry };
-    let primitive_compiler = PrimitiveCompiler::new(config);
-    (GenericCompiler::default(), (primitive_compiler,))
-}
--- crates/compiler/src/prim.rs
+++ crates/compiler/src/prim.rs
@@ -1,9 +1,9 @@
 use std::{
     any::{Any, TypeId},
-    path::PathBuf,
     sync::Arc,
 };
 
+use luminair_air::{calculate_log_size, gen_add_trace};
 use luminal::prelude::*;
 
 use crate::data::StwoData;
@@ -21,29 +21,28 @@ impl PrimitiveCompiler {
 }
 
 #[derive(Debug, Default, PartialEq)]
-pub struct Config {
-    pub trace_registry: Option<PathBuf>,
-}
+pub struct Config {}
 
 // ====== BINARY ======
 #[derive(Debug, Clone, Default, PartialEq)]
-pub struct StwoAdd {
+pub struct Add {
     node_id: usize,
     config: Arc<Config>,
 }
 
-impl StwoAdd {
+impl Add {
     pub fn new(node_id: usize, config: Arc<Config>) -> Self {
         Self { node_id, config }
     }
 }
 
-impl Operator for StwoAdd {
+impl Operator for Add {
     fn process(&mut self, inp: Vec<(InputTensor, ShapeTracker)>) -> Vec<Tensor> {
         if inp.len() != 2 {
             panic!("Add operator requires exactly two input tensors.");
         }
 
+        // Get data
         let (a_tensor, _a_shape) = &inp[0];
         let (b_tensor, _b_shape) = &inp[1];
 
@@ -53,14 +52,22 @@ impl Operator for StwoAdd {
             } else if let Some(data) = tensor.borrowed().downcast_ref::<StwoData>() {
                 StwoData(Arc::clone(&data.0))
             } else {
-                panic!("Unsupported input type for StwoAdd");
+                panic!("Unsupported input type for Add");
             }
         };
 
-        let _a_data = get_data(a_tensor);
-        let _b_data = get_data(b_tensor);
+        let a = get_data(a_tensor);
+        let b = get_data(b_tensor);
 
-        todo!()
+        // Calculate required trace size based on tensor dimensions
+        let max_size = a.0.len().max(b.0.len());
+        let log_size = calculate_log_size(max_size);
+
+        // Generate trace and get result tensor
+        let (_trace, c) = gen_add_trace(log_size, &a.0, &b.0);
+
+        let c = vec![Tensor::new(StwoData(Arc::new(c)))];
+        c
     }
 }
 
@@ -76,13 +83,13 @@ impl Compiler for PrimitiveCompiler {
             let op = graph.node_weight(id).unwrap().as_any().type_id();
             let op_ref = graph.graph.node_weight_mut(id).unwrap();
 
-            if is::<Add>(op) {
-                *op_ref = Box::new(StwoAdd::new(id.index(), Arc::clone(&self.config)))
-            } else if is::<Mul>(op) {
+            if is::<luminal::op::Add>(op) {
+                *op_ref = Box::new(Add::new(id.index(), Arc::clone(&self.config)))
+            } else if is::<luminal::op::Mul>(op) {
                 todo!()
-            } else if is::<Contiguous>(op) {
+            } else if is::<luminal::op::Contiguous>(op) {
                 *op_ref = Box::new(Contiguous)
-            } else if is::<Function>(op) {
+            } else if is::<luminal::op::Function>(op) {
                 // Keep the Function operator as is
                 continue;
             } else {
